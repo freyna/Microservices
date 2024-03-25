2187ce85414dff7ce83a3ba9fce3ba3a9a957005
Filiberto Reyna <filiberto.reyna@senderrarx.com>
2024-03-25 17:44:56 -0600

    Add project files.

---
+++ Servicios.Api.Datos/Entities/Autor.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Servicios.Api.Datos.Entities
+{
+    public class Autor
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Nombre")]
+        public string Nombre { get; set; }
+
+        [BsonElement("Apellido")]
+        public string Apellido { get; set; }
+
+        [BsonElement("GradoAcademico")]
+        public string GradoAcademico { get; set; }
+        
+    }
+}
