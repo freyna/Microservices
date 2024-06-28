09aa018d32bba7bae7b7e5db08e0f8c5878ad9dd
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2024-06-27 18:07:14 -0600

    Implementing security

---
+++ Servicios.Api.Seguridad/Core/Persistence/SeguridadData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Servicios.Api.Seguridad.Core.Entities;
+
+namespace Servicios.Api.Seguridad.Core.Persistence
+{
+    public class SeguridadData
+    {
+        public static async Task InsertarUsuario(SeguridadContexto context, UserManager<Usuario> usuarioManager)
+        {
+            var exists = await usuarioManager.Users.AnyAsync();
+
+            if (!exists)
+            {
+                var usuario = new Usuario
+                {
+                    Nombre = "Filiberto",
+                    Apellido = "Reyna",
+                    Direccion = "Ribera 130 Zempoala",
+                    UserName = "freyna",
+                    Email = "ing.Filiberto.Reyna@gmail.com"
+                };
+
+                await usuarioManager.CreateAsync(usuario,"Password123$");
+            }
+        }
+    }
+}
--- Servicios.Api.Seguridad/Program.cs
+++ Servicios.Api.Seguridad/Program.cs
@@ -1,13 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Servicios.Api.Seguridad.Core.Entities;
+using Servicios.Api.Seguridad.Core.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<SeguridadContexto>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ConectionDB"));
+});
+
+builder.Services.AddIdentityCore<Usuario>()
+    .AddEntityFrameworkStores<SeguridadContexto>()
+    .AddApiEndpoints();
+
 var app = builder.Build();
+
+using (var context = app.Services.CreateScope())
+{
+    var services = context.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<Usuario>>();
+        var contextEF = services.GetRequiredService<SeguridadContexto>();
+
+        SeguridadData.InsertarUsuario(contextEF, userManager).Wait();
+    }
+    catch (Exception ex)
+    {
+        var logging = services.GetRequiredService<ILogger<Program>>();
+        logging.LogError(ex, "Error al registrar usuario");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +53,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapIdentityApi<Usuario>();
 app.Run();
